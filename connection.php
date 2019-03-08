7791d634481e5e23a759d313a4e3432e38b5aa8e
1808234 <1808234@rgu.ac.uk>
2019-03-08 13:03:30 +0000

    try

--- connection.php
+++ connection.php
@@ -5,6 +5,12 @@ $dbusername="1808234";
 $dbpassword="1808234";
 $dbname="";
 
+// localhost connection information:
+$local="localhost";
+$user="root";
+$pass="";
+$DB_name= "users";
+
 $db = mysqli_connect('$dbhost','$dbusername','$dbpassword','$dbname');
 if($db-> connect_error) {
     die('Error'.('.$db->connect_errno.'));
