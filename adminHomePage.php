77ea6f8f711b5b11e48cc039ce42afdcb6021b2c
1808234 <1808234@rgu.ac.uk>
2019-03-26 18:23:03 +0000

    confirm things

--- adminHomePage.php
+++ adminHomePage.php
@@ -51,83 +51,88 @@ if ( !isset( $_SESSION['user'])) {
 
     <article>
         <?php
-        if (isset($_POST['add'])){
-            // here the admin can add a new user when click add BUTTON
-            print "<form action='adminJobs/add.php' method='post'>
-                        <div>
-                             <div >
-                                 <label>First Name</label>
-                                 <input id='first' type='text' name='first' placeholder='first name' required>
-                             </div>
-                             
-                             <div >
-                             <label>Last Name</label>
-                               <input id='last' type='text' name='last' placeholder='last name' required>                                
-                             </div>
-                             
-                            <div >
-                             <label>Username</label>
-                               <input type='text' id='user' name='username' placeholder='username' required>                               
-                             </div>
-                             
-                             <div >
-                             <label>Email</label>
-                               <input id='email' type='email' name='email' placeholder='email' >    
-                             </div>
-                             
-                               <div class='input-box'>
-                                <label>Password</label>
-                               <input id='pass1' type='password' name='password' placeholder='password' >  
-                             </div>                  
-                             
-                             <div class='input-box'>
-                                <label>Confirm Password</label>
-                               <input id='pass2' type='password' name='password_con' placeholder='confirm password' >                                
-                             </div>
-                             
-                             <div>
-                                 <label>Type of User</label>
-                                            <select name='user_type'>
-                                            <option></option>
-                                            <option>Add User</option>
-                                            <option>Add Administrator</option>
-                                 </select>
-                            </div>
+        if (isset($_POST['add'])){   // here the admin can add a new user when click add BUTTON?>
 
-                        <button class='input-bt' id='add_u' type='submit' name='add_user'>Add New User</button>
-                      
-                        </div>
-                  </form>
-                  
-                  <form action='adminJobs/close.php' method='post'>
-                    <div>
-                         <button style='background: red' class='input-bt' type='submit' onclick='my()' name='close'>Cancel</button>
+            <form action='adminJobs/add.php' method='post'>
+                <div>
+                    <div >
+                        <label>First Name</label>
+                        <input id='first' type='text' name='first' placeholder='first name' required>
                     </div>
-                </form>";
 
+                    <div >
+                        <label>Last Name</label>
+                        <input id='last' type='text' name='last' placeholder='last name' required>
+                    </div>
+
+                    <div >
+                        <label>Username</label>
+                        <input type='text' id='user' name='username' placeholder='username' required>
+                    </div>
+
+                    <div >
+                        <label>Email</label>
+                        <input id='email' type='email' name='email' placeholder='email' >
+                    </div>
+
+                    <div class='input-box'>
+                        <label>Password</label>
+                        <input id='pass1' type='password' name='password' placeholder='password' >
+                    </div>
+
+                    <div class='input-box'>
+                        <label>Confirm Password</label>
+                        <input id='pass2' type='password' name='password_con' placeholder='confirm password' >
+                    </div>
+
+                    <div>
+                        <label>Type of User</label>
+                        <select name='user_type'>
+                            <option></option>
+                            <option>Add User</option>
+                            <option>Add Administrator</option>
+                        </select>
+                    </div>
+
+                    <button class='input-bt' id='add_u' type='submit' name='add_user'>Add New User</button>
+
+                </div>
+            </form>
+
+            <form onsubmit="return confirm('Are Sure you?')"  action='adminJobs/close.php' method='post'>
+                <div>
+                    <button style='background: red' class='input-bt' type='submit'  name='close'>Cancel</button>
+                </div>
+            </form>
+
+<?php
         }
         // delete form for the admin delete a usr from the database
-        if (isset($_POST['delete'])){
-            echo "<form action='adminJobs/delete.php' method='post'>                        
-                          <div class='input-box'>
-                                <label>Username/email</label>
-                                <input type='text' name='de' placeholder='username/email that want to delete' required>
-                              
-                          </div>
-                                <button onclick='del()' type='submit' id='delete' name='delete' class='input-bt' >Delete</button>
-                       <script>
-                             function del() {
-                             return alert('user been deleted');
-                             }
-                        </script> 
-                       <!-- // closing button or cancel-->      
-                    </form>
-                  <form action='adminJobs/close.php' method='post'>
-                    <div>
-                         <button style='background: red' class='input-bt' type='submit' onclick=' my()' name='close'>Cancel</button>
-                    </div>
+        if (isset($_POST['delete'])){?>
 
-                    </form>";
+            <form action='adminJobs/delete.php' method='post'>
+                <div class='input-box'>
+                    <label>Username/email</label>
+                    <input type='text' name='de' placeholder='username/email that want to delete' required>
+
+                </div>
+                <button onclick='del()' type='submit' id='delete' name='delete' class='input-bt' >Delete</button>
+                <script>
+                    function del() {
+                        return alert('user been deleted');
+                    }
+                </script>
+                <!-- // closing button or cancel-->
+            </form>
+
+            <form onsubmit="return confirm('Are Sure you?')"  action='adminJobs/close.php' method='post'>
+                <div>
+                    <button style='background: red' class='input-bt' type='submit' name='close'>Cancel</button>
+                </div>
+
+            </form>
+
+          <?php
         }
 
         ?>
